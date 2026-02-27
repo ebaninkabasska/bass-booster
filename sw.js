b9f29c027f959d4b555884c8b2a30312e065bb79
ebaninkabasska <egorshrek@gmail.com>
2026-02-27 18:21:55 +0300

    roblox

--- sw.js
+++ sw.js
@@ -1,19 +1,41 @@
+const CACHE_VERSION = 'bass-booster-v2';
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('bass-booster-v1').then((cache) => {
+    caches.open(CACHE_VERSION).then((cache) => {
       return cache.addAll([
         './',
         './index.html',
         './manifest.json'
       ]);
-    })
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_VERSION) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+    fetch(e.request).then((response) => {
+      const responseClone = response.clone();
+      caches.open(CACHE_VERSION).then((cache) => {
+        cache.put(e.request, responseClone);
+      });
+      return response;
+    }).catch(() => {
+      return caches.match(e.request);
     })
   );
 });
